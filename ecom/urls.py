7e345fc69d02550b6249160e4064a1162629d7b4
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-11-08 12:16:43 +0530

    Update urls.py

--- ecom/urls.py
+++ ecom/urls.py
@@ -7,14 +7,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
-
-    # The Django admin is not officially supported; expect breakage.
-    # Nonetheless, it's often useful for debugging.
-
     path('admin/', admin.site.urls),
-
     path('', include(apps.get_app_config('oscar').urls[0])),
-    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
